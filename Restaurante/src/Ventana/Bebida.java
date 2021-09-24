03ccedcb46bf0fc7a60ca5653973729b754b2f36
emanuel soto <manusl2908@gmail.com>
2021-09-24 00:02:40 -0600

    agregando clase bebida con sus atributos y metodos basicos

---
+++ Restaurante/src/Ventana/Bebida.java
@@ -0,0 +1,39 @@
+
+package Ventana;
+
+
+public class Bebida {
+    private String nombre;
+    private double precio;
+
+    public Bebida(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Bebida{nombre=").append(nombre);
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
