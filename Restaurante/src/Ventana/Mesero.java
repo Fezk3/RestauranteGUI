650ece87af9e2e51eb010f5793d2650e95f8b86a
Victor Segura <seguravictor57@gmail.com>
2021-09-27 16:35:05 -0600

    nueva lista mesasAtencion en mesero y metodo para aniadirlas

--- Restaurante/src/Ventana/Mesero.java
+++ Restaurante/src/Ventana/Mesero.java
@@ -1,14 +1,23 @@
 package Ventana;
 
+import java.util.ArrayList;
+
 public class Mesero extends Persona {
     
     
     private double salario;
-  // private List<Mesa> mesasAtencion; falta clase mesa
+    private ArrayList<Integer> mesasAtencion;
 
     public Mesero(String nombre, String apellido, String celular, double salario) {
         super(nombre, apellido, celular);
         this.salario = salario;
+        mesasAtencion = new ArrayList<Integer>();
+    }
+    
+    public void recibeMesa(int numMesa){
+        
+        mesasAtencion.add(numMesa);  // id de la mesa
+        
     }
 
     public double getSalario() {
