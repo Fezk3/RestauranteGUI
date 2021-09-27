9c27cd4980a6c3b6f311068732f3a9537f5ea464
emanuel soto <manusl2908@gmail.com>
2021-09-27 17:02:45 -0600

    agregando variable a cliente

--- Restaurante/src/Ventana/Cliente.java
+++ Restaurante/src/Ventana/Cliente.java
@@ -5,12 +5,14 @@ public class Cliente extends Persona{
     private String direccion;
     private String tipoDePago;
     private String servicioDeAtencion;
+    private boolean pagado;
 
     public Cliente(String direccion, String tipoDePago, String servicioDeAtencion, String nombre, String apellido, String celular) {
         super(nombre, apellido, celular);
         this.direccion = direccion;
         this.tipoDePago = tipoDePago;
         this.servicioDeAtencion = servicioDeAtencion;
+        
     }
 
     public String getDireccion() {
@@ -36,13 +38,22 @@ public class Cliente extends Persona{
     public void setServicioDeAtencion(String servicioDeAtencion) {
         this.servicioDeAtencion = servicioDeAtencion;
     }
+
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
     
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Persona{direccion=").append(direccion);
+        sb.append("Cliente{direccion=").append(direccion);
         sb.append(", tipoDePago=").append(tipoDePago);
         sb.append(", servicioDeAtencion=").append(servicioDeAtencion);
+        sb.append(", pagado=").append(pagado);
         sb.append(", Persona=").append(super.toString());
         sb.append('}');
         return sb.toString();
