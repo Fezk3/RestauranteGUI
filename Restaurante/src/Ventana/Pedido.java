7408839293702b2b1005e490d6b8e46d66461b0d
emanuel soto <manusl2908@gmail.com>
2021-09-27 16:49:08 -0600

    modificando clase pedido

--- Restaurante/src/Ventana/Pedido.java
+++ Restaurante/src/Ventana/Pedido.java
@@ -6,29 +6,35 @@ import java.util.ArrayList;
 
 public class Pedido {
     private ArrayList <Bebida> LBebida =new ArrayList <Bebida>();
-    // private ArrayList <Comida> LComida =new ArrayList <Comida>();
+    private ArrayList <Comida> LComida =new ArrayList <Comida>();
+    private String cliente;
+    private String mesero;
 
-    public Pedido() {
+    public Pedido(String cliente, String mesero) {
+        this.cliente=cliente;
+        this.mesero=mesero;
     }
     public void agregarBebida(Bebida bebida){
         LBebida.add(bebida);
     }
     
-    /*public void agregarComida(Comida comida){
+    public void agregarComida(Comida comida){
         LComida.add(comida);
-    }*/
+    }
     
     public void eliminarBebida(Bebida bebida){
         LBebida.remove(bebida);
     }
     
-    /*public void eliminarComida(Comida comida){
+    public void eliminarComida(Comida comida){
         LComida.remove(comida);
-    }*/
+    }
 
     public ArrayList<Bebida> getLBebida() {
         return LBebida;
     }
     
-    
+    public ArrayList<Comida> getLComida() {
+        return LComida;
+    }
 }
