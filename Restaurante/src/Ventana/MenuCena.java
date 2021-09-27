8d7e43044634e0d1d7772db9e9516c13ab072aef
Victor Segura <seguravictor57@gmail.com>
2021-09-27 17:38:26 -0600

    cambio de nombre a metodo en menuCena para que calce y format al file

--- Restaurante/src/Ventana/MenuCena.java
+++ Restaurante/src/Ventana/MenuCena.java
@@ -3,33 +3,35 @@ package Ventana;
 import java.util.ArrayList;
 
 public class MenuCena {
-    
+
     public ArrayList<Comida> cenas;
 
     public MenuCena() {
-        
+
         this.cenas = new ArrayList<Comida>();
-        
+
     }
-    
-    public void agegarCena(Comida nueva){
-        
+
+    public void agegarCena(Comida nueva) {
+
         cenas.add(nueva);
-        
+
     }
-    
-    public void generarCenasBase(){
-        
+
+    public void generarCenasBase() {
+
         Comida pizza = new Comida("Pizza", "Cena", 5000);
         Comida spaghetti = new Comida("Spaghetti", "Cena", 2500);
         Comida sopa = new Comida("Sopa Azteca", "Cena", 3000);
         Comida hamburguesa = new Comida("Hamburguesa", "Cena", 4000);
         Comida vigoron = new Comida("Vigoron", "Cena", 4500);
-        
-        cenas.add(pizza);   cenas.add(spaghetti);
-        cenas.add(sopa);    cenas.add(hamburguesa);
+
+        cenas.add(pizza);
+        cenas.add(spaghetti);
+        cenas.add(sopa);
+        cenas.add(hamburguesa);
         cenas.add(vigoron);
-        
+
     }
-    
+
 }
