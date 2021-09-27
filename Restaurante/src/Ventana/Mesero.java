be417fa38d0fe1df6f3043f5eb9f6d48638d918c
Victor Segura <seguravictor57@gmail.com>
2021-09-27 16:27:23 -0600

    cambios en mesero, ahora hereda bien

--- Restaurante/src/Ventana/Mesero.java
+++ Restaurante/src/Ventana/Mesero.java
@@ -2,43 +2,13 @@ package Ventana;
 
 public class Mesero extends Persona {
     
-    private String nombre, apellido, celular;
+    
     private double salario;
   // private List<Mesa> mesasAtencion; falta clase mesa
 
     public Mesero(String nombre, String apellido, String celular, double salario) {
         super(nombre, apellido, celular);
         this.salario = salario;
-    }
-
-    @Override
-    public String getNombre() {
-        return nombre;
-    }
-
-    @Override
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public String getApellido() {
-        return apellido;
-    }
-
-    @Override
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    @Override
-    public String getCelular() {
-        return celular;
-    }
-
-    @Override
-    public void setCelular(String celular) {
-        this.celular = celular;
     }
 
     public double getSalario() {
@@ -52,9 +22,9 @@ public class Mesero extends Persona {
         @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Persona{nombre=").append(nombre);
-        sb.append(", apellido=").append(apellido);
-        sb.append(", celular=").append(celular);
+        sb.append("Persona{nombre=").append(super.getNombre());
+        sb.append(", apellido=").append(super.getApellido());
+        sb.append(", celular=").append(super.getCelular());
         sb.append(", salario=").append(salario);
         sb.append(", Persona=").append(super.toString());
         sb.append('}');
