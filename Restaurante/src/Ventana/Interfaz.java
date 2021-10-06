2dd72a297eae270cd3d43138db66740c9a2f8dac
emanuel soto <manusl2908@gmail.com>
2021-10-05 21:22:21 -0600

    haciendo cambios en lista de mesas

--- Restaurante/src/Ventana/Interfaz.java
+++ Restaurante/src/Ventana/Interfaz.java
@@ -17,19 +17,19 @@ public class Interfaz {
     public Interfaz() {
 
         meseros = new ArrayList<Mesero>();
-        mesas = new ArrayList<Mesa>();
+        mesas = new ArrayList<Mesa>(12);
         clientes = new ArrayList<Cliente>();
         facturas = new ArrayList<Factura>();
         menuBebida = new MenuBebida();
         menuAlmuerzo = new MenuAlmuerzo();
         menuCena = new MenuCena();
-        menuDesayuno =new MenuDesayuno();
-
+        menuDesayuno = new MenuDesayuno();
+        llenarListaMesa();
     }
 
     public void agregarComida(Comida nueva) {
-        
-        if(nueva.getTipo().equals("Desayuno")){
+
+        if (nueva.getTipo().equals("Desayuno")) {
             menuDesayuno.agegarComida(nueva);
         }
         if (nueva.getTipo().equals("Almuerzo")) {
@@ -38,36 +38,41 @@ public class Interfaz {
         if (nueva.getTipo().equals("Cena")) {
             menuCena.agegarCena(nueva);
         }
-        if(nueva.getTipo().equals("Bebida")){
+        if (nueva.getTipo().equals("Bebida")) {
             menuBebida.agegarBebida(nueva);
         }
 
     }
-    
-    public void llenaMeseros(){
-        
+
+    public void llenaMeseros() {
+
         meseros.add(new Mesero("Julio", "Perez", "85878986", 15000));
         meseros.add(new Mesero("Jose", "Pereira", "85878986", 15000));
         meseros.add(new Mesero("Mario", "Rodriguez", "85878986", 15000));
         meseros.add(new Mesero("Marvin", "Calvo", "85878986", 15000));
-        
-    }
-    
-    public boolean desocuparMesas(int n){
-        
-        for(Mesa mes: mesas){
-            
-            if(mes.getNumero_mesa() == n && mes.getDisponible() == false){
-                
-                mes.desocuparMesa();
-                return true;
-                
-            }
-            
-        }
-        
-        return false;
-        
+
     }
 
+    public boolean desocuparMesas(int n) {
+
+        for (Mesa mes : mesas) {
+
+            if (mes.getNumero_mesa() == n && mes.getDisponible() == false) {
+
+                mes.desocuparMesa();
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    private void llenarListaMesa() {
+        for (int i = 0; i < 12; i++) {
+            mesas.add(new Mesa(i,""));
+        }
+    }
 }
